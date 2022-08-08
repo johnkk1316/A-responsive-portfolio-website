00736455212c3e7923c6fa17f898ab78a8722f96
John KK <codeona4@gmail.com>
2022-08-09 01:34:54 +0300

    Added menu show Y hidden

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+/*---MENU SHOW Y HIDDEN----*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
