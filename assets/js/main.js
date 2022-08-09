d41f0feebaea4b4b081e27a90d0fc9403b4ebc5a
John KK <codeona4@gmail.com>
2022-08-09 05:23:32 +0300

    Added menu hidden and validated if contant exists

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,9 +7,19 @@ const navMenu = document.getElementById('nav-menu'),
 
 
 /*---ADDED MENU SHOW---*/
-/*---VALIDATE IF CONSTANT EXISTS.---  */
+/*---VALIDATE IF CONSTANT EXISTS.---*/
 if(navToggle){
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
+    })
+}
+
+
+
+/*---ADDED MENU HIDDEN---*/
+/*---VALIDATE IF CONSTANT EXISTS.---*/
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
     })
 }
