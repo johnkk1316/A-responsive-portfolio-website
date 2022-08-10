b0d62d1eb15c05825d12e45952d6048bf36d8723
John KK <codeona4@gmail.com>
2022-08-10 10:54:02 +0300

    Added accordian skill for skill toggle purposes

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,7 +26,7 @@ if(navClose){
 
 
 
-/*REMOVE MENU MOBILE*/
+/*----REMOVE MENU MOBILE----*/
 const navLink = document.querySelectorAll('.nav_link')
 
 function linkAction(){
@@ -35,3 +35,27 @@ function linkAction(){
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+
+/*---ACCORDION SKILLS---*/
+const   skillsContent = document.getElementsByClassName('skills_content'),
+        skillsHeader  = document.querySelectorAll('.skills_header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills_content skills_close'
+    }
+    if(itemClass === 'skills_content skills_close'){
+        this.parentNode.className = 'skills_content skills_open'
+    }
+}
+
+
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+})
